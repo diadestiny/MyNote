51f24843499a7745a02cf486916e5cd54dbe9d81
diadestiny <1247889154@qq.com>
2019-07-10 23:16:37 +0800

    2019.7.10

--- java/接口回调/Client.java
+++
@@ -1,9 +0,0 @@
-package com.diadestiny.test002;
-
-public class Client implements MyCallInterface {
-	@Override
-	public void printName() {
-		// TODO Auto-generated method stub
-		System.out.println("this is the client printName method");
-	}
-}
--- java/接口回调/MyCallInterface.java
+++
@@ -1,5 +0,0 @@
-package com.diadestiny.test002;
-
-public interface MyCallInterface {
-	public void printName();
-}
